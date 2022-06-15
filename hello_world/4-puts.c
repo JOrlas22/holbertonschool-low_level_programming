6e1ff6e06994e071942973c03572e5047692ce1d
JOrlas22 <josedavidtrice@gmail.com>
2022-06-15 11:20:27 -0700

    gcc

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,3 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c "Programming is like building a multilingual puzzle
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c
+"Programing is like building a multilingual puzzle
