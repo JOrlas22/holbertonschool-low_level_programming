aa935653b77e4225ea500ebe36550c8987fd8674
JOrlas22 <josedavidtrice@gmail.com>
2022-06-15 11:15:31 -0700

    gcc

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,2 @@
-#include <stdio.h>
+#!/bin/bash
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
-int main(void)
-{
-	puts("\""Programming is like building a multilingual puzzle");
-	return (0);
-}
